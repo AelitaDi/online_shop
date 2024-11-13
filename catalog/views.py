dc29103f98bfbc32094b1404cbb4851379cc227e
uliana rogova <uv.rogova@yandex.ru>
2024-11-13 13:48:56 +0300

    additional task 22

--- catalog/views.py
+++ catalog/views.py
@@ -1,3 +1,4 @@
+from django.http import HttpResponse
 from django.shortcuts import render
 
 
@@ -6,4 +7,9 @@ def home(request):
 
 
 def contacts(request):
+    if request.method == "POST":
+        name = request.POST.get("name")
+        phone = request.POST.get("phone")
+        message = request.POST.get("message")
+        return HttpResponse(f"Спасибо, {name.title()}! Сообщение получено.")
     return render(request, 'contacts.html')
